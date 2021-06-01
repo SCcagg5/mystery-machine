96b58c8098aef35e8a009ae29508a81c75540ac2
SCcagg5 <eliot@LAPTOP-C7G3T243.localdomain>
2021-06-01 19:16:32 +0200

    adding code and readme

---
+++ morning_sunshine/README.md
@@ -0,0 +1,8 @@
+## Moring sunshine
+
+On crée un object dont la fonction `main` sera appellée
+On envoie a l'object l'argument numéro 1, ce dernier est "load" durant l'initialisation
+
+Les batiments sont d'Ouest en Est on comprend donc que pour avoir une vue sur le soleil situé a l'Est le matin il faut que le / less batiment precedants ne soit pas plus haut que l'etage actuel (seul les etages de tailles strictement superieures seront donc concernés par cette augementation de prix)
+On parcours ensuite la tableau en gardant en mémoire le batiment de la rangée le plus haut
+Nous ajoutons dans une variables les benefices cumulés
--- morning_sunshine/morning_sunshine.py
+++ morning_sunshine/morning_sunshine.py
@@ -3,23 +3,21 @@ import json
 import math
 
 class Morning_sunshine:
-    def __init__(self, file):
+    def __init__(self, data):
         self.file = file
-        self.data = None
+        self.data = json.loads(data)
         self.benefices = 0
 
-    def read(self):
-        f = open(self.file)
-        self.data = json.load(f)
-        f.close()
-
     def main(self):
-        for batiment in self.data:
+        max = 0
+        for batiment in self.data[::-1]:
             benef = 0
             for appartement in batiment["floor_layout"]:
                 if 'E' in appartement['orientations']:
-                    benef += math.floor(appartement['monthly_rent'] * 0.05)
-                    benef *= batiment['height']
+                    if batiment['height'] > max:
+                        benef += math.floor(appartement['monthly_rent'] * 0.05)
+                        benef *= batiment['height'] - max
+                        max = batiment['height']
             self.benefices += benef
         return self.benefices
 
